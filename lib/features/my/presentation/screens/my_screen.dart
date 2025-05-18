821c3dea032c6eeabe6a51f2ffbccffa6719aff0
ibrahem137 <ibrahemalhuossien@gmail.com>
2025-05-18 09:52:14 +0300

    added our screen

---
+++ lib/features/my/presentation/screens/my_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyScreen extends StatelessWidget {
+  const MyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'we  created our first repo successfully',
+        ),
+      ),
+    );
+  }
+}
